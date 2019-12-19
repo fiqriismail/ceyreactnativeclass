8593e3cc210eae4e087b57b259cacc6ef86270c6
fiqriismail <fiqri@peachit.lk>
2019-12-19 11:56:45 +0530

    Layout Horizontal

--- App.js
+++ App.js
@@ -5,10 +5,9 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.f30}>Hello World</Text>
-        <Text style={styles.f15}>
-          The quick brown fox jumped over the lazy dog.
-        </Text>
+        <View style={styles.redContainer}></View>
+        <View style={styles.greenContainer}></View>
+        <View style={styles.yellowContainer}></View>
       </View>
     );
   }
@@ -17,15 +16,18 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  redContainer: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  greenContainer: {
+    flex: 8,
+    backgroundColor: 'green',
+  },
+  yellowContainer: {
+    flex: 1,
     backgroundColor: 'yellow',
-    alignItems: 'center',
-  },
-  f30: {
-    fontSize: 30,
-  },
-  f15: {
-    fontSize: 15,
   },
 });
 
