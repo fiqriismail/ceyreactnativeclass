f4872ba5d10d3b8f1add603b0baf723dc38ee45e
fiqriismail <fiqri@peachit.lk>
2019-12-19 11:47:19 +0530

    Seperated CSS

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 class App extends Component {
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          backgroundColor: 'yellow',
-          alignItems: 'center',
-        }}>
-        <Text style={{fontSize: 30}}>Hello World</Text>
-        <Text style={{fontSize: 15}}>
+      <View style={styles.container}>
+        <Text style={styles.f30}>Hello World</Text>
+        <Text style={styles.f15}>
           The quick brown fox jumped over the lazy dog.
         </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+  },
+  f30: {
+    fontSize: 30,
+  },
+  f15: {
+    fontSize: 15,
+  },
+});
 
 export default App;
