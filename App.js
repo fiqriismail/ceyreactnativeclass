59048b494216811cf70eb7c10163285b5ba2231a
fiqriismail <fiqri@peachit.lk>
2019-12-19 16:53:18 +0530

    React native paper

--- App.js
+++ App.js
@@ -1,14 +1,80 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import React, {Component} from 'react';
+import {View, TextInput, StyleSheet, ScrollView} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {Avatar, Button, Card, Title, Paragraph} from 'react-native-paper';
 
-import HomeScreen from './HomeScreen';
-import About from './About';
+class App extends Component {
+  state = {};
 
-const mainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  About: {screen: About},
+  render() {
+    return (
+      <PaperProvider>
+        <ScrollView>
+          <Card>
+            <Card.Title
+              title="Card Title"
+              subtitle="Card Subtitle"
+              left={props => <Avatar.Icon {...props} icon="folder" />}
+            />
+            <Card.Content>
+              <Title>Card title</Title>
+              <Paragraph>Card content</Paragraph>
+            </Card.Content>
+            <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
+            <Card.Actions>
+              <Button>Cancel</Button>
+              <Button>Ok</Button>
+            </Card.Actions>
+          </Card>
+          <Card>
+            <Card.Title
+              title="Card Title"
+              subtitle="Card Subtitle"
+              left={props => <Avatar.Icon {...props} icon="folder" />}
+            />
+            <Card.Content>
+              <Title>Card title</Title>
+              <Paragraph>Card content</Paragraph>
+            </Card.Content>
+            <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
+            <Card.Actions>
+              <Button>Cancel</Button>
+              <Button>Ok</Button>
+            </Card.Actions>
+          </Card>
+          <Card>
+            <Card.Title
+              title="Card Title"
+              subtitle="Card Subtitle"
+              left={props => <Avatar.Icon {...props} icon="folder" />}
+            />
+            <Card.Content>
+              <Title>Card title</Title>
+              <Paragraph>Card content</Paragraph>
+            </Card.Content>
+            <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
+            <Card.Actions>
+              <Button>Cancel</Button>
+              <Button>Ok</Button>
+            </Card.Actions>
+          </Card>
+        </ScrollView>
+      </PaperProvider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textbox: {
+    borderWidth: 1,
+    margin: 5,
+    width: 300,
+  },
 });
-
-const App = createAppContainer(mainNavigator);
 
 export default App;
